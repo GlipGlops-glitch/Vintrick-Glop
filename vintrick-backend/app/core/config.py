a7451732013b36b3de5cb13147db7523986eee94
Higgins <Casey.Higgins@smwe.com>
2025-07-29 17:20:36 -0700

    CH

--- vintrick-backend/app/core/config.py
+++ vintrick-backend/app/core/config.py
@@ -4,9 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    DATABASE_URL: str = os.getenv(
-        "DATABASE_URL",
-        "mssql+pyodbc://harvest_user:h@rvest_smwe123!@SMWECSMDEV10/Higgins_Sandbox?driver=ODBC+Driver+18+for+SQL+Server"
-    )
+    # Only read from environment, no hardcoded fallback!
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
--- vintrick-backend/app/core/db.py
+++ vintrick-backend/app/core/db.py
@@ -1,12 +1,9 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+from app.core.config import settings
 
-load_dotenv()
-
-DATABASE_URL = "mssql+pyodbc://harvest_user:h@rvest_smwe123!@SMWECSMDEV10/Higgins_Sandbox?driver=ODBC+Driver+18+for+SQL+Server"
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
